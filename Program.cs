7a86c420621ae4a42c5c2026a49f328f3bf4e16a
vinegarboy <necogusa398@gmail.com>
2021-06-03 10:16:11 +0900

    1フレーム目のみ保存する機能の作成

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
+using System.IO;
 namespace Opencv
 {
     class Program
     {
         static void Main(string[] args)
         {
-			string path;
+			string path,savepath;
 			VideoCapture vc;
+			Mat mp = new Mat();
+			DirectoryInfo di;
 			Console.WriteLine("動画のパスを入力してください。");
 			path=Console.ReadLine();
 			Console.WriteLine($"このパスでよろしいですか？大丈夫な場合は1を入力してください。\n{path}");
@@ -17,9 +20,16 @@ namespace Opencv
 				path=Console.ReadLine();
 				Console.WriteLine($"このパスでよろしいですか？大丈夫な場合は1を入力してください。\n{path}");
 			}
+			savepath=$"./saves/{Path.GetFileNameWithoutExtension(path)}";
+			di = new DirectoryInfo(savepath);
 			vc = new VideoCapture(path);
 			for(int i = 0;i<vc.FrameCount;i++){
+				if(i!=0){
 
+				}else{
+					vc.Read(mp);
+					BitmapConverter.ToBitmap(mp).Save($"{savepath}/{i}.jpg");
+				}
 			}
         }
     }
