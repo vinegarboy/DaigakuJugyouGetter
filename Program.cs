4159630f88822628d7d0570f59a53ecff5dc388e
vinegarboy <necogusa398@gmail.com>
2021-06-03 10:25:28 +0900

    保存処理完成

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using System.Globalization;
+using System;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
+using System.Drawing;
 using System.IO;
 namespace Opencv
 {
     class Program
     {
+		public bool Compare(Bitmap bp1,Bitmap bp2){
+
+			return true;
+		}
         static void Main(string[] args)
         {
 			string path,savepath;
 			VideoCapture vc;
-			Mat mp = new Mat();
+			Mat mt = new Mat(),_mt = new Mat();
 			DirectoryInfo di;
 			Console.WriteLine("動画のパスを入力してください。");
 			path=Console.ReadLine();
@@ -24,11 +30,16 @@ namespace Opencv
 			di = new DirectoryInfo(savepath);
 			vc = new VideoCapture(path);
 			for(int i = 0;i<vc.FrameCount;i++){
+				vc.PosFrames = i;
 				if(i!=0){
-
+					vc.Read(_mt);
+					if(Compare(BitmapConverter.ToBitmap(mt),BitmapConverter.ToBitmap(_mt))){
+						mt = _mt;
+						BitmapConverter.ToBitmap(mt).Save($"{savepath}/{i}.jpg");
+					}
 				}else{
-					vc.Read(mp);
-					BitmapConverter.ToBitmap(mp).Save($"{savepath}/{i}.jpg");
+					vc.Read(mt);
+					BitmapConverter.ToBitmap(mt).Save($"{savepath}/{i}.jpg");
 				}
 			}
         }
